0ba09bd010190529240c4c66e7654c57e03fdd96
Mutasim Fouad Showmik <124583421+Fouad-Showmik@users.noreply.github.com>
2024-11-29 21:14:16 +0600

    main.py

--- main.py
+++ main.py
@@ -8,39 +8,6 @@ print(response.json())
 # #
 # #
 # #
-# # #To check valid proxies..
-# #
-# #
-# #
-import threading
-import requests
-import queue
-
-q=queue.Queue()
-valid_proxies=[]
-
-with open('proxy.txt','r') as file:
-    proxies=file.read().split('\n')
-    for i in proxies:
-        q.put(i)
-
-def check_proxy():
-    global q
-    while not q.empty():
-        proxy=q.get()
-        try:
-            response=requests.get('https://ipinfo.io/json',
-                                  proxies={'http':proxy,'https':proxy})
-        except:
-            continue
-        if response.status_code==200:
-            print(proxy)
-for j in range(10):
-    threading.Thread(target=check_proxy).start()
-    
-
-# #
-# #
 # ####################################################
 # #
 # #
